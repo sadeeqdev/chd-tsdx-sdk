ba34baf96c1ea1f2ab2195f23ec227b13e400acf
Abubakar Ibrahim <88150129+sadeeqdev@users.noreply.github.com>
2024-01-13 05:01:53 +0100

    Update constants.js

--- app/constants.js
+++ app/constants.js
@@ -1,4 +1,3 @@
 export const address = "0xB27595Bedd063935ca146EB46ee7CaE40F696f7E";
 export const contractAddress = "0xB27595Bedd063935ca146EB46ee7CaE40F696f7E";
-export const private_key =
-  "5c175f36f6cf2e2c3688223b3484e02eb2d64b78e08eb44d9a915fce9a5cf190";
+
